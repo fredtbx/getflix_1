9cd01709edf8a291129352462eb2407571fa7aab
Fred Tarbox <fredtbx@Freds-MacBook-Pro.local>
2018-04-27 10:13:44 -0400

    First Commit

---
+++ js/ClientApp.jsx
@@ -0,0 +1,20 @@
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    {
+      id: 'my-first-component'
+    },
+    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'Rick & Morty', color: 'LimeGreen' }),
+    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+  );
+};
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('App'));
